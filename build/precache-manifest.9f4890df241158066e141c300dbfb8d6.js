1922a409c3c95df40c9b09fbf9538642edad746e
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2019-03-30 12:33:06 -0600

    removed /build from .gitignore

---
+++ build/precache-manifest.9f4890df241158066e141c300dbfb8d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c3bd79ed0e125202499",
+    "url": "/MemoryGame/static/js/runtime~main.d54f135b.js"
+  },
+  {
+    "revision": "d5bc160d6177651d3e0a",
+    "url": "/MemoryGame/static/js/main.5dbc9ded.chunk.js"
+  },
+  {
+    "revision": "b73afbb738c4fda25a1a",
+    "url": "/MemoryGame/static/js/2.d327fbf7.chunk.js"
+  },
+  {
+    "revision": "d5bc160d6177651d3e0a",
+    "url": "/MemoryGame/static/css/main.6f37bc4d.chunk.css"
+  },
+  {
+    "revision": "5ab58264f2e5603fcbfb421638331ffa",
+    "url": "/MemoryGame/index.html"
+  }
+];
