8b65a842107bb3e6906393ffdcb8cf84177561f4
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2019-04-01 13:07:43 -0600

    updates to footer.js

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,8 +4,7 @@ import "./footer.css";
 // set the footer with copyright and github link
 const Footer = props => (
   <div className="footer">
-    // eslint-disable-next-line react/style-prop-object
-    <p class="text-muted text-center">Copyright &copy; All rights reserved | made with <i class="icon-heart" aria-hidden="true" style="color: purple;"></i> by <a href="https://github.com/ChereeNielson" target="_blank" id="#">Cheree Nielson</a></p>
+    <p class="text-muted text-center">Copyright &copy; All rights reserved | made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://github.com/ChereeNielson" target="_blank" rel="noreferrer noopener" id="#">Cheree Nielson</a></p>
   </div>
 );
 
